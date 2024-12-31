bea3d98a4a18b3bd1bab0baf7f4bd86a24a94063
Joseph Awe <130408142+Jossyboydgenius@users.noreply.github.com>
2024-12-31 15:32:59 +0000

    Fix typo in import path and update home screen reference from OnbodingScreen to OnboardingScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:rive_animation/screens/onboding/onboding_screen.dart';
+import 'package:rive_animation/screens/onboarding/onboarding_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
           errorBorder: defaultInputBorder,
         ),
       ),
-      home: const OnbodingScreen(),
+      home: const OnboardingScreen(),
     );
   }
 }
